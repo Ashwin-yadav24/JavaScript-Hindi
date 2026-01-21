55ad57138e5384aa750f452c1d306b9a3613bbe5
Ashwin-yadav24 <ashwinyadav1420@gmail.com>
2026-01-21 22:37:46 +0530

    Added JavaScript data types example

---
+++ 01_Basics/02.DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs,not browser
+
+console.log(3+3) 
+console.log("Ashwin")
+
+let name="ashwin"
+let age=3
+let isLOggedIn= true
+
+let state; //undefined
+
+let estate=null //(empty)
+
+
+// number => 2 to pwr of 53
+// bigint 
+// string => " "
+// Boolean => true/false
+// null 
+// undefined =>
+// symbol => unique
+
+//object 
+
+console.log(typeof "hitesh")
+console.log(typeof null)
