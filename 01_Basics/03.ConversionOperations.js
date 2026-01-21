6fe6e96d1aa31ac2f7f2e6b3e3835bfc7fd6f6e8
Ashwin-yadav24 <ashwinyadav1420@gmail.com>
2026-01-21 22:48:52 +0530

    Added conversion operations example in JavaScript

---
+++ 01_Basics/03.ConversionOperations.js
@@ -0,0 +1,23 @@
+let score="hitesh"
+console.log(typeof score)
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" ==>33
+// "33abc" =>Nan
+// true => 1; flase=>0
+
+let isLoggedIn=1
+let boolIsLoggeIn=Boolean(isLoggedIn)
+console.log(boolIsLoggeIn)
+
+// 1=> true; 0 => false;
+// "" => false
+// "ashwin" => true
+
+let sumNum=33
+let stringNum=String(sumNum)
+console.log(stringNum)
+console.log(typeof stringNum)
