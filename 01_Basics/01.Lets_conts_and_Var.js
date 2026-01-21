b1b1c22fec7a6d1ef03ae1d321a8eb1ef5c7d24a
Ashwin-yadav24 <ashwinyadav1420@gmail.com>
2026-01-21 22:15:33 +0530

    Updated let, const and var examples

---
+++ 01_Basics/01.Lets_conts_and_Var.js
@@ -0,0 +1,11 @@
+const accountId=2020
+let accountName ="Ashwin"
+var accountPassword="admin@123"
+accountCity="Jaipur"
+
+console.log("Account ID:", accountId)
+console.log("Account Name:", accountName)
+console.log("Account Password:", accountPassword)
+console.log("Account City:", accountCity)
+console.table([ accountId, accountName, accountPassword, accountCity ]);
+
