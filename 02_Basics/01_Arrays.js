a90a349e61def60d49ab4d06b47e3c892f3fb1d7
Ashwin-yadav24 <ashwinyadav1420@gmail.com>
2026-04-10 00:57:11 +0530

    Array file updated

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -5,26 +5,26 @@ const myHeros = ["shaktiman", "naagraj", "doga"];
 
 const myArry2 = new Array(1,2,3,4,5)
 
-// console.log(myArr[0]);
+console.log(myArr[0]);
 
 
 
 // Array Methods
 
-// myArr.push(6)
-// myArr.pop()
+myArr.push(6)
+myArr.pop()
 
-// myArr.unshift(0)
-// myArr.shift()
+myArr.unshift(0)
+myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
-// console.log(myArr);
-// console.log(newArr);
-// console.log(typeof(newArr));
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+console.log(typeof(newArr));
 
 // Slice and Splice
 
